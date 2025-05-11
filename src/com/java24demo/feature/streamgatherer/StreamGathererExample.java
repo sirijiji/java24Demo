07a7149926dbd43a028f427d8a53d466a137ddad
sirijiji <sirisak_01@yahoo.fr>
2025-05-11 18:20:12 +0200

    [java24] add stream gatherer example

--- src/com/java24demo/feature/streamgatherer/StreamGathererExample.java
+++ src/com/java24demo/feature/streamgatherer/StreamGathererExample.java
@@ -12,24 +12,28 @@ public class StreamGathererExample {
         Stream<String> fruits = Stream.of("apple", "apricot", "banana", "blueberry", "cherry");
 
         Gatherer<String, HashMap<String, StringBuilder>, Map<String, String>> groupByFirstLetter =
-                Gatherer.of(HashMap<String, StringBuilder>::new,
-                        (map, fruit, downstream) -> {
-                            IO.println("Adding fruit: " + fruit);
+                Gatherer.of(HashMap<String, StringBuilder>::new,                                                    //initializer
+                        (map, fruit, downstream) -> {    // integrator
+                            IO.println("Adding fruit: " + fruit + " to private state object map");
                             String key = fruit.substring(0, 1);
-                            map.computeIfAbsent(key, k -> new StringBuilder()).append(fruit).append(", ");
+                            StringBuilder stringBuilder = map.computeIfAbsent(key, k -> new StringBuilder());
+                            stringBuilder.append(fruit).append(", ");
                             return true;
 
-                        }, (map1, map2) -> map1,
-                        (map, downstream) -> {
+                        }, (map1, map2) -> map1,            // combiner
+                        (map, downstream) -> {               // finisher
+
                             Map<String, String> result = new HashMap<>();
+                            IO.println("Removing trailing comma for each value");
                             map.forEach((key, value) -> {
-                                // Retirer la dernière virgule et l'espace
                                 String str = value.toString();
                                 if (str.endsWith(", ")) {
                                     str = str.substring(0, str.length() - 2);
                                 }
+
                                 result.put(key, str);
                             });
+                            IO.println("Adding the map to downstream");
                             downstream.push(result);
                         }
                 );
